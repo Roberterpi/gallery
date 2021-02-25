713dd83ca3c539157bf50433e2d8fdf5e2964393
roberterpi <robertpopct@gmail.com>
2021-02-25 14:41:32 +0200

    added function previous

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,16 @@ const nodes = document.querySelectorAll(".gallery-img");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 
+function previous() {
+  nextBtn.disabled = false;
+  nodes[currentlySelected].classList.remove("active");
+  currentlySelected--;
+  nodes[currentlySelected].classList.add("active");
+
+  if (currentlySelected === 0) {
+    prevBtn.disabled = true;
+  }
+}
 
 function init() {
   prevBtn.addEventListener("click", function() {
