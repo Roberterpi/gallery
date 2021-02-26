733f4820badbda6bdcc9542d7c90d57ffd2b949b
roberterpi <robertpopct@gmail.com>
2021-02-26 18:51:02 +0200

    added prev btn in js

--- js/gallery.js
+++ js/gallery.js
@@ -4,7 +4,14 @@ const galerryImgs = document.querySelectorAll(".gallery-img");
 let currentlySelected = 0;
 
 prevBtn.addEventListener('click', function() {
+  galerryImgs[currentlySelected].classList.remove('active');
+  currentlySelected--; //currentlySelected = currentlySelected - 1
+  galerryImgs[currentlySelected].classList.add('active');
+  nextBtn.disabled = false;
 
+  if (currentlySelected === 0) {
+    prevBtn.disabled = true;
+  }
 });
 
 nextBtn.addEventListener('click', function() {
